20321540e65a2afd926d87d04118dadc05118083
vita <v.bakatina@gmail.com>
2023-09-06 15:00:33 +0300

    -added key for project

--- FirstShop/settings.py
+++ FirstShop/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY', default='')
+# SECRET_KEY = config('SECRET_KEY', default='')
+SECRET_KEY = 'django-insecure-h5^yp19bjfs8e^!y9)09kkdmia762-9r+tlv(34yggeosn4fh5'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
